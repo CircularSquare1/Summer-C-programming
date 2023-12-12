82ff26da13566197c4640f23d8d54ddeb29bed9d
CircularSquare1 <153470184+CircularSquare1@users.noreply.github.com>
2023-12-12 10:20:32 +0000

    ToDo listshift.c

---
+++ listShift.c
@@ -0,0 +1,11 @@
+// Task is to implement a function that shifts a linked list to the right n times and returns a 
+// pointer to the start of the updated list. The nopde at the end of the list is moved to the start.
+
+
+#include <stdio.h> 
+
+struct node *shift(struct node *list, int n);
+
+int main () {
+
+}
